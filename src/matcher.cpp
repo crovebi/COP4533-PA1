b8ac4aa04face07bac10ab0e6b137f741df050d1
crovebi <s00ubway@gmail.com>
2026-01-30 14:26:42 -0500

    added gale shapely algorithm implementation

--- src/matcher.cpp
+++ src/matcher.cpp
@@ -20,7 +20,48 @@ std::vector<std::vector<int>> preferenceInput(std::ifstream& file, int n) {
 }
 
 std::vector<int> deferredAcceptance(std::vector<std::vector<int>> hospital_preference, std::vector<std::vector<int>> student_preference, int n) {
-
+    std::vector<int> hospital_matching(n, -1);
+    std::vector<int> student_matching(n, -1);
+    std::vector<int> next_proposal(n, 0);
+    
+    std::vector<int> hospital_available;
+    for(int h = 0; h < n; h++) {
+        hospital_available.push_back(h);
+    }
+    
+    while(!hospital_available.empty()) {
+        int h = hospital_available.back();
+        hospital_available.pop_back();
+        
+        while(hospital_matching[h] == -1 && next_proposal[h] < n) {
+            int s = hospital_preference[h][next_proposal[h]];
+            next_proposal[h]++;
+            
+            if(student_matching[s] == -1) {
+                hospital_matching[h] = s;
+                student_matching[s] = h;
+            } 
+            else {
+                int current_hospital = student_matching[s];
+                int new_pos = -1, current_pos = -1;
+                for(int k = 0; k < n; k++) {
+                    if (student_preference[s][k] == h) new_pos = k;
+                    if (student_preference[s][k] == current_hospital) current_pos = k;
+                    if (new_pos != -1 && current_pos != -1) break;
+                }
+                
+                if(new_pos < current_pos) {
+                    hospital_matching[current_hospital] = -1;
+                    hospital_available.push_back(current_hospital);
+                    
+                    hospital_matching[h] = s;
+                    student_matching[s] = h;
+                }
+            }
+        }
+    }
+    
+    return hospital_matching;
 }
 
 int main() {
